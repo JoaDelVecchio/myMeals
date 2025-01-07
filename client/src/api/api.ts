a3b2e1f10e82a1fdcee1af2ce4c479d7361152f7
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2025-01-07 13:06:20 -0300

    Add env variable

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -3,7 +3,7 @@ import axios, { AxiosError } from "axios";
 import { Meal } from "../types/meal";
 
 export const getMeals = async (): Promise<Meal[]> => {
-  const API_BASE_URL = "http://localhost:8000/api/meals";
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
   try {
     const response = await axios.get(API_BASE_URL);
     return response.data; // Return the data
