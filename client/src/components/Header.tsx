7843b76adcba635197496081f141dc03fd84fd72
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2025-01-08 15:41:47 -0300

    Change title

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Logo from "../assets/Logo.png";
 
 const Header = ({
@@ -15,36 +15,38 @@ const Header = ({
 
   return (
     <header className="flex justify-between items-center bg-gray-100 text-gray-800 p-4 md:p-6">
-      <Link to="/" className="flex items-center space-x-3">
+      <NavLink to="/" className="flex items-center space-x-3">
         <img src={Logo} alt="ProteinPlates Logo" className="w-10 h-10" />
         <h1 className="text-2xl font-bold">ProteinPlates</h1>
-      </Link>
+      </NavLink>
 
       {/* Search Input */}
-      <div className="relative w-64">
-        <input
-          type="text"
-          name="mealSearched"
-          placeholder="What do you want to eat?"
-          value={mealSearched}
-          onChange={(e) => handleMealSearched(e)}
-          className="w-full p-2 pl-10 text-black rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="absolute left-3 top-2.5 h-5 w-5 text-gray-400"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M8 16l-4 4m0 0l4-4m-4 4h16M8 8l4-4m0 0l-4 4m4-4H4"
+      <NavLink to="/">
+        <div className="relative w-64">
+          <input
+            type="text"
+            name="mealSearched"
+            placeholder="What do you want to eat?"
+            value={mealSearched}
+            onChange={(e) => handleMealSearched(e)}
+            className="w-full p-2 pl-10 text-black rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-        </svg>
-      </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="absolute left-3 top-2.5 h-5 w-5 text-gray-400"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M8 16l-4 4m0 0l4-4m-4 4h16M8 8l4-4m0 0l-4 4m4-4H4"
+            />
+          </svg>
+        </div>
+      </NavLink>
 
       {/* Navigation */}
       <nav>
